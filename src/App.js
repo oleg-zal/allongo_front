788a1507607a8800703de9784c10f211df4428db
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-14 13:33:06 +0100

    Step 4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import ProductListPage from "./pages/ProductListPage";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import ProtectedRoutesComponent from './components/ProtectedRoutesComponent'
+import UserProfilePage from "./pages/user/UserProfilePage";
+import UserOrdersPage from "./pages/user/UserOrdersPage";
+import UserCartDetailsPage from "./pages/user/UserCartDetailsPage";
+import UserOrderDetailsPage from "./pages/user/UserOrderDetailsPage";
 
 function App() {
   return (
@@ -19,6 +24,15 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="*" element="Page not exists 404" />
         {/* <Route path="/" component={HomePage} />  in previous versions of react-router-dom */}
+        <Route element={<ProtectedRoutesComponent />}>
+          <Route path="/user" element={<UserProfilePage />} />
+          <Route path="/user/my-orders" element={<UserOrdersPage />} />
+          <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
+          <Route
+            path="/user/order-details"
+            element={<UserOrderDetailsPage />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
