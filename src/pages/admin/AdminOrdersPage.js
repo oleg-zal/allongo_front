ab8fe62824f15b30382fcd257ec9cbe7e1fa5011
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-15 10:18:14 +0100

    Step 45 (53. Create Component for Admin Links)

--- src/pages/admin/AdminOrdersPage.js
+++ src/pages/admin/AdminOrdersPage.js
@@ -1,10 +1,12 @@
 import { Row, Col, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import AdminLinksComponent from "../../components/admin/AdminLinksComponent";
+
 const AdminOrdersPage = () => {
   return (
     <Row className="m-5">
         <Col md={2}>
-        admin links
+        <AdminLinksComponent />
         </Col>
       <Col md={10}>
         <h1>Orders</h1>
