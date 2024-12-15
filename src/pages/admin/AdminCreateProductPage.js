93c3c64fce8b9533c883e5afbec32844e6d71540
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-15 10:41:59 +0100

    Step 49 (57. Finish the Page for Creating The Product by Admin)

--- src/pages/admin/AdminCreateProductPage.js
+++ src/pages/admin/AdminCreateProductPage.js
@@ -1,4 +1,13 @@
-import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Container,
+  Form,
+  Button,
+  CloseButton,
+  Table,
+  Alert,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -50,7 +59,10 @@ const AdminCreateProductPage = () => {
               <Form.Control name="price" required type="text" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCategory">
-              <Form.Label>Category</Form.Label>
+              <Form.Label>
+                Category
+                <CloseButton />(<small>remove selected</small>)
+              </Form.Label>
               <Form.Select
                 required
                 name="category"
@@ -62,6 +74,98 @@ const AdminCreateProductPage = () => {
                 <option value="3">Games</option>
               </Form.Select>
             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicNewCategory">
+              <Form.Label>
+                Or create a new category (e.g. Computers/Laptops/Intel){" "}
+              </Form.Label>
+              <Form.Control name="newCategory" type="text" />
+            </Form.Group>
+
+            <Row className="mt-5">
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicAttributes">
+                  <Form.Label>Choose atrribute and set value</Form.Label>
+                  <Form.Select
+                    name="atrrKey"
+                    aria-label="Default select example"
+                  >
+                    <option>Choose attribute</option>
+                    <option value="red">color</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicAttributeValue"
+                >
+                  <Form.Label>Attribute value</Form.Label>
+                  <Form.Select
+                    name="atrrVal"
+                    aria-label="Default select example"
+                  >
+                    <option>Choose attribute value</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>Attribute</th>
+                    <th>Value</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>attr key</td>
+                    <td>attr value</td>
+                    <td>
+                      <CloseButton />
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Row>
+
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicNewAttribute">
+                  <Form.Label>Create new attribute</Form.Label>
+                  <Form.Control
+                    disabled={false}
+                    placeholder="first choose or create category"
+                    name="newAttrValue"
+                    type="text"
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicNewAttributeValue"
+                >
+                  <Form.Label>Attribute value</Form.Label>
+                  <Form.Control
+                    disabled={false}
+                    placeholder="first choose or create category"
+                    required={true}
+                    name="newAttrValue"
+                    type="text"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Alert variant="primary">
+              After typing attribute key and value press enterr on one of the
+              field
+            </Alert>
+
             <Form.Group controlId="formFileMultiple" className="mb-3 mt-3">
               <Form.Label>Images</Form.Label>
 
